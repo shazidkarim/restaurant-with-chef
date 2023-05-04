bf8dba9623f57243a471c46839c6e4833e15858c
shazidkarim <shazid747@gmail.com>
2023-05-04 18:51:05 +0600

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
     errorElement:<ErrorPage></ErrorPage>,
     children:[
       {
-        path:"/home",
+        path:"/",
         element:<Home></Home>
       },
       {
