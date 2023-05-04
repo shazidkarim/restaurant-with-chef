da629a4e6949b6ff70f14098362cc590b0c98b41
shazidkarim <shazid747@gmail.com>
2023-05-04 15:33:22 +0600

    welcome section added

---
+++ src/components/welcomeBanner/WelcomeBanner.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {  Container } from 'react-bootstrap';
+
+const WelcomeBanner = () => {
+  return (
+  
+      <Container className='border mt-4'>
+        <h1 className='text-center text-primary mb-4'>Welcome to our Restaurant</h1>
+        <p className='text-center'>
+        We welcome you to a delicious feast of exquisite dishes in Star line. With a wide range of world cuisines to choose from, we guarantee you a sumptuous feast experience in our restaurant. 
+        </p>
+      </Container>
+    
+  );
+};
+
+export default WelcomeBanner;
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -6,14 +6,16 @@ import Banner from '../../components/Banner/Banner';
 import ChefDetails from '../../components/ChefDetails/ChefDetails';
 import Footer from '../../components/Footer/Footer';
 import OurJourney from '../../components/OurJourney/OurJourney';
+import WelcomeBanner from '../../components/welcomeBanner/WelcomeBanner';
 
 const Home = () => {
     
     return (
         <div>
             <Banner></Banner>
-            <OurJourney></OurJourney>
+            <WelcomeBanner></WelcomeBanner>
             <ChefDetails></ChefDetails>
+            <OurJourney></OurJourney>
             <Footer></Footer>
         </div>
     );
